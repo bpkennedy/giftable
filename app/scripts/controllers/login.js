6bb6e71a2e4d4fc7058ab25b0ee2c5f06eb384be
kennedyb <brian.kennedy@wwt.com>
2016-01-06 21:33:15 -0600

    This is a fix for the login bug and the login error message display bug.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -34,7 +34,9 @@ angular.module('giftableApp')
       }
 
       function createProfile(user) {
-        var ref = Ref.child('users', user.uid), def = $q.defer();
+         console.log('inside createProfile');
+         console.log(user.uid);
+        var ref = Ref.child('users/' + user.uid), def = $q.defer();
         ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
           $timeout(function() {
             if( err ) {
@@ -67,7 +69,27 @@ angular.module('giftableApp')
     }
 
     function showError(err) {
-      $scope.err = err;
+        if (err) {
+            switch (err.code) {
+              case 'INVALID_EMAIL':
+                $scope.err = 'Email is invalid.';
+                break;
+              case 'INVALID_PASSWORD':
+                $scope.err = 'Password incorrect.';
+                break;
+              case 'INVALID_USER':
+                $scope.err = 'User account does not exist.';
+                break;
+              case 'EMAIL_TAKEN':
+                $scope.err = 'Email already taken.';
+                break;
+              case 'NETWORK_ERROR':
+                $scope.err = 'An error occurred while attempting to contact the authentication server.';
+                break;
+              default:
+                $scope.err = 'Error logging user in:' + err;
+            }
+        }
     }
 
 
